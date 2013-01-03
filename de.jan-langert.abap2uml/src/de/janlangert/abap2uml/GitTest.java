0a8ab114fa5320335c5434fdd00f55679184a4a8
Jan Langert <info@jan-langert.de>
2013-01-03 23:14:19 +0100

    Initial commit

---
+++ de.jan-langert.abap2uml/src/de/janlangert/abap2uml/GitTest.java
@@ -0,0 +1,13 @@
+package de.janlangert.abap2uml;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Git Step..");
+	}
+
+}
